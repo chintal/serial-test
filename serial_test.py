0eaebc9381f2b1df201c2d9d4e4f8fdc64919212
Chintalagiri Shashank <shashank@chintal.in>
2016-07-17 18:54:30 +0530

    Minor cleanup.

--- serial_test.py
+++ serial_test.py
@@ -30,8 +30,9 @@ def dump_status():
     else:
         byer = 0.00
 
-    print (str(datetime.datetime.now()) + '::' + str(throughput) + ' bytes/sec :: BYER :' + str(byer) +
-            " :: Total :" + str(total_recieved_bytes) + " :: Errors :" + str(total_error_bytes))
+    print '{0}::{1} bytes/sec :: BYER :{2} :: Total :{3} :: Errors :{4}' \
+          ''.format(datetime.datetime.now(), throughput, byer,
+                    total_recieved_bytes, total_error_bytes)
 
 
 def update_status():
@@ -56,17 +57,25 @@ def update_status():
     threading.Timer(next_update - time.time(), update_status).start()
 
 
-def begintest(layer2):
+def init_prbs():
+    global prbs
+    prbs = _prbs.new_lfsr16_t()
+    _prbs.lfsr_vInit(prbs, int(_prbs.LFSR_DEFAULT_SEED), int(_prbs.LFSR_DEFAULT_TAPS))
+    return
+
+
+def begin_ber_test(layer2):
     global recieved_bytes
     global error_bytes
     global prbs
     init_prbs()
-    layer2.write('a')
+    layer2.write('b')
     expected_char = _prbs.lfsr_cGetNextByte(prbs)
     sync = 0
+    print "Waiting for Sync"
     while not sync:
         char = layer2.read()
-        if char == chr(0x01):
+        if char == 'b':
             sync = 1
             print "Synced"
     global next_update
@@ -81,20 +90,40 @@ def begintest(layer2):
         expected_char = _prbs.lfsr_cGetNextByte(prbs)
 
 
-def init_prbs():
-    global prbs
-    prbs = _prbs.new_lfsr16_t()
-    _prbs.lfsr_vInit(prbs, int(_prbs.LFSR_DEFAULT_SEED), int(_prbs.LFSR_DEFAULT_TAPS))
-    return
+def begin_throughput_test(layer2):
+    global recieved_bytes
+    global error_bytes
+    layer2.write('a')
+    expected_char = ord('0')
+    sync = 0
+    print "Waiting for Sync"
+    while not sync:
+        char = layer2.read()
+        if char == 'a':
+            sync = 1
+            print "Synced"
+    global next_update
+    next_update = time.time()
+    update_status()
+    while 1:
+        char = layer2.read()
+        if ord(char) != expected_char:
+            error_bytes += 1
+        recieved_bytes += 1
+        if expected_char is ord('z'):
+            expected_char = ord('0')
+        else:
+            expected_char += 1
+
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM4', timeout=1)
+    ser = serial.Serial('/dev/ttyACM1', baudrate=256000, timeout=1)
     ser.flushInput()
     ser.flushOutput()
     i = 0
     drain = ser.read()
-    drain = []
     while len(drain) == 1:
         drain = ser.read()
         i += 1
-    begintest(ser)
+    print "Drained {0} bytes".format(i)
+    begin_throughput_test(ser)
